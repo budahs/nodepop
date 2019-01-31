51297e2297efbd2e4d70b2168cbf4003ff9479ee
Pablo Garcia <budaya.anim@gmail.com>
2019-01-31 02:09:37 +0100

    Added validations for query parameters

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const Add = require('../models/Add');
+const validations = require('../lib/validations');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try {         
+        const filters = validations(req.query);
+        const start = parseInt(req.query.start);
+        const limit = parseInt(req.query.limit);  
+        const sort =  req.query.sort;     
+        let sortHow='';
+        if(sort){
+            let asc=1;
+            if(desc == 1)asc=-1;
+            sortHow = [[sort,asc]];
+        }  
+        const adds = await Add.list(filters,sortHow,start,limit);
+        res.locals.title = 'Nodepop a simple API exercise'; 
+        res.locals.advertisements = adds;
+        res.render('index');
+    }catch(err){
+        next(err);
+        return;
+    } 
 });
 module.exports = router;
